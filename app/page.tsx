c256b8b6ff1f65ca09b4182bc80087081dbfb2e0
maywascold <stepanovigor2110@gmail.com>
2025-05-18 12:06:31 +0300

    mobile + tablet

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Quiz from "../components/Quiz";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div
-      className="grid grid-rows-[20px_1fr_20px] items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-white"
+      className="grid grid-rows-[20px_1fr_20px] items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-white bg-image"
       style={{
         backgroundImage: "url('./bg.jpg')",
         backgroundSize: "100%",
@@ -20,7 +21,8 @@ export default function Home() {
 
       <main className="flex flex-col gap-[32px] row-start-2 sm:items-start z-10 relative container mx-auto">
         <div className="flex flex-col md:flex-row gap-8 items-center">
-          <div className="flex flex-col max-w-md bg-white bg-opacity-90 p-6 rounded-lg shadow-lg">
+          <div className="flex flex-col md:max-w-md bg-white bg-opacity-90 p-6 rounded-lg shadow-lg max-w-full">
+            <div><Image className="md:hidden mb-5" src="/bg.jpg" alt="Description of image" width={500} height={300} /></div>
             <h1 className="text-3xl text-gray-600 font-bold mb-4">
               Live Busy status
             </h1>
